fedf4fb059317a9a1a3d07006dc1c9d1965d9f2b
Anmoldeep kaur <anmolchahal2002@gmail.com>
2026-02-22 18:12:19 +0530

    Create quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,52 @@
+/*
+================================================================================
+Quality Checks
+================================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrityc, consistency,
+    and accuracy of the Gold layer, These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Run these checks after loading Gold layer.
+    - Investigate and resolve any discrepencies found during the checks.
+================================================================================
+*/
+
+-- =============================================================================
+-- Checking 'gold.customer_key'
+-- =============================================================================
+-- Checking for uniqueness of Customer key in gold.dim_customers
+-- Expectation : No Results
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- =============================================================================
+-- Checking 'gold.product_key'
+-- =============================================================================
+-- Checking for uniqueness of Preoduct key in gold.dim_products
+-- Expectation : No Results
+SELECT
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- =============================================================================
+-- Checking 'gold.fact_sales'
+-- =============================================================================
+-- Checking the data model connectivity between fact and dimensions
+SELECT * 
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_customers c
+ON c.customer_key =f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL;
